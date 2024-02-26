b55ffe0a5c5cb61f76508b3d8900dc78e8953fa1
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-26 00:39:43 -0400

    Modelo de Tickets Realizado

---
+++ TicketsDetallesWA/BibliotecaModels/Models/Tickets.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModels.Models
+{
+    public class Tickets
+    {
+
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es Obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Este campo es Obligatorio")]
+        [ForeignKey("Prioridades")]
+        public int PrioridadId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Este campo es Obligatorio")]
+        [ForeignKey("Clientes")]
+        public int ClienteId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Este campo es Obligatorio")]
+        [ForeignKey("Sistemas")]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Por favor, introduce un Nombre válido")]
+        public string? Solicitadopor { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Por favor, introduce un Asunto válido")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Por favor, introduce una Descripciòn válida")]
+        public string? Descripcion { get; set; }
+
+        public Tickets()
+        {
+            Fecha = DateTime.Now;
+        }
+    }
+}
