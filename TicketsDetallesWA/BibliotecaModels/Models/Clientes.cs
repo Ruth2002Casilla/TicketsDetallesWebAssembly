407c2a47e622164f5864ab90730d2bb3e1d03108
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-25 21:00:06 -0400

    Agregado el Modelo del Cliente en la Biblioteca

--- TicketsDetallesWA/BibliotecaModels/Models/Clientes.cs
+++ TicketsDetallesWA/BibliotecaModels/Models/Clientes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,39 @@ namespace BibliotecaModels.Models
 {
     internal class Clientes
     {
+        [Key]
+        public int ClienteId { get; set; }
 
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Por favor, introduce un Nombre válido")]
+        public string? NombreCliente { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Por favor, introduce un número de Teléfono válido")]
+        [Range(0, int.MaxValue, ErrorMessage = "El número de Teléfono no puede ser negativo")]
+        public int TelefonoCliente { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Por favor, introduce un número de Celular válido")]
+        [Range(0, int.MaxValue, ErrorMessage = "El número del Celular no puede ser negativo")]
+        public int CelularCliente { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "Por favor, introduce un RNC válido")]
+        public string? RNCCliente { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Por favor, introduce un Correo Electrónico válido")]
+        public string? EmailCliente { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-Z][a-zA-Z0-9\s]*$", ErrorMessage = "Por favor, introduce una Dirección válida")]
+        public string? DireccionCliente { get; set; }
     }
 }
