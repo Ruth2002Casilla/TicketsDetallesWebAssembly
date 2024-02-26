6d24628fe780fb9a69c565150fd423cf74227ab0
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-26 10:02:22 -0400

    Creando el Modelo de Sistemas

---
+++ TicketsDetallesWA/BibliotecaModels/Models/Sistema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModels.Models
+{
+    public class Sistema
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Por favor, introduce un Nombre válido")]
+        public string? Nombre { get; set; }
+    }
+}
