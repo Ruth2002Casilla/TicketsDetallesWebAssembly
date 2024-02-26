e26620baf05e59d87634eeed185c72e736ce13dc
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-26 00:57:41 -0400

    Agregando el Moelo de TicketsDetalles

---
+++ TicketsDetallesWA/BibliotecaModels/Models/TicketsDetalles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModels.Models
+{
+    public class TicketsDetalles
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Emisor { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Mensaje { get; set; }
+
+        public TicketsDetalles() { }
+
+        public TicketsDetalles(int id, string emisor, string mensaje)
+        {
+            Id = id;
+            Emisor = emisor;
+            Mensaje = mensaje;
+        }
+    }
+}
